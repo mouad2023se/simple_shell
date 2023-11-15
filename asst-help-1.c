00307dd9b2e2888354d9192434755a59212917cf
doudowa <arifidouaa6@gmail.com>
2023-11-15 09:23:31 -0800

    ext-var-chek.c

---
+++ asst-help-1.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+* get_help - Display help information for the shell commands.
+* @datash: Shell data structure.
+* Return: Always returns 1.
+*/
+int get_help(data_shell *datash)
+{
+
+	/* Check if a specific command is provided */
+	if (datash->args[1] == 0)
+		aux_help_general();
+	else if (_strcmp(datash->args[1], "setenv") == 0)
+		aux_help_setenv();
+	else if (_strcmp(datash->args[1], "env") == 0)
+		aux_help_env();
+	else if (_strcmp(datash->args[1], "unsetenv") == 0)
+		aux_help_unsetenv();
+	else if (_strcmp(datash->args[1], "help") == 0)
+		aux_help();
+	else if (_strcmp(datash->args[1], "exit") == 0)
+		aux_help_exit();
+	else if (_strcmp(datash->args[1], "cd") == 0)
+		aux_help_cd();
+	else if (_strcmp(datash->args[1], "alias") == 0)
+		aux_help_alias();
+	else
+		write(STDERR_FILENO, datash->args[0],
+		      _strlen(datash->args[0]));
+
+	/* Set the status to 0 */
+	datash->status = 0;
+	/* Always return 1 */
+	return (1);
+}
