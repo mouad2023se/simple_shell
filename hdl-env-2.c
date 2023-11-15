118ab78b2b98e3df27b8b25fc95d02728154fa7b
mouad2023se <elharrakmouad@gmail.com>
2023-11-15 08:11:25 -0800

    simple shell

---
+++ hdl-env-2.c
@@ -0,0 +1,136 @@
+#include "shell.h"
+
+/**
+* copy_info - Cp & concatenate the name and value to create a new env var strg
+* @name: Name of the environment variable.
+* @value: Value of the environment variable.
+* Return: Newly allocated string containing "name=value"
+*/
+
+char *copy_info(char *name, char *value)
+{
+	char *new;
+	int len_name, len_value, len;
+
+	len_name = _strlen(name);
+	len_value = _strlen(value);
+	len = len_name + len_value + 2;
+
+	/* Allocate memory for the new string */
+
+	new = malloc(sizeof(char) * (len));
+
+	/* Copy the name, concatenate '=', and copy the value */
+
+	_strcpy(new, name);
+	_strcat(new, "=");
+	_strcat(new, value);
+	_strcat(new, "\0");
+
+	return (new);
+}
+
+/**
+* set_env - Set the value of an envir var or create a new 1 if not exists.
+* @name: Name of the environment variable.
+* @value: Value to set.
+* @datash: Shell data structure.
+*/
+
+void set_env(char *name, char *value, data_shell *datash)
+{
+	int i;
+	char *var_env, *name_env;
+
+	/* Iterate through existing environment variables */
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		var_env = _strdup(datash->_environ[i]);
+		name_env = _strtok(var_env, "=");
+
+		/* Check if the environment variable with the given name already exists */
+
+		if (_strcmp(name_env, name) == 0)
+		{
+			free(datash->_environ[i]);
+			datash->_environ[i] = copy_info(name_env, value);
+			free(var_env);
+			return;
+		}
+		free(var_env);
+	}
+
+	/* If the environment variable doesn't exist, create a new one */
+
+	datash->_environ = _reallocdp(datash->_environ, i, sizeof(char *) * (i + 2));
+	datash->_environ[i] = copy_info(name, value);
+	datash->_environ[i + 1] = NULL;
+}
+
+/**
+* _setenv - Set the value of an environment variable or create a new one.
+* @datash: Shell data structure.
+* Return: Always returns 1.
+*/
+int _setenv(data_shell *datash)
+{
+
+	if (datash->args[1] == NULL || datash->args[2] == NULL)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+
+	set_env(datash->args[1], datash->args[2], datash);
+
+	return (1);
+}
+
+/**
+* _unsetenv - Remove an environment variable.
+* @datash: Shell data structure.
+* Return: Always returns 1.
+*/
+int _unsetenv(data_shell *datash)
+{
+	char **realloc_environ;
+	char *var_env, *name_env;
+	int i, j, k;
+
+	if (datash->args[1] == NULL)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+	k = -1;
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		var_env = _strdup(datash->_environ[i]);
+		name_env = _strtok(var_env, "=");
+		if (_strcmp(name_env, datash->args[1]) == 0)
+		{
+			k = i;
+		}
+		free(var_env);
+	}
+	if (k == -1)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+	realloc_environ = malloc(sizeof(char *) * (i));
+	for (i = j = 0; datash->_environ[i]; i++)
+	{
+		if (i != k)
+		{
+			realloc_environ[j] = datash->_environ[i];
+			j++;
+		}
+	}
+	realloc_environ[j] = NULL;
+	free(datash->_environ[k]);
+	free(datash->_environ);
+	datash->_environ = realloc_environ;
+	return (1);
+}
