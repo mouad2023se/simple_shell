bebd89cdfa43c3b2914df8820962bd5dfb784b5b
mouad2023se <elharrakmouad@gmail.com>
2023-11-15 07:14:04 -0800

    simple shell

---
+++ var-chk-synt.c
@@ -0,0 +1,169 @@
+#include "shell.h"
+
+/**
+* repeated_char - Count the number of repeated characters in a row.
+* @input: Input string.
+* @i: Current index in the string.
+* Return: Number of repeated characters.
+*/
+
+int repeated_char(char *input, int i)
+{
+	if (*(input - 1) == *input)
+		return (repeated_char(input - 1, i + 1));
+
+	return (i);
+}
+
+/**
+* error_sep_op - Check for syntax errors related to separators.
+* @input: Input string.
+* @i: Current index in the string.
+* @last: Last encountered separator.
+* Return: Index where the error occurs (if any).
+*/
+int error_sep_op(char *input, int i, char last)
+{
+	int count;
+
+	count = 0;
+	if (*input == '\0')
+		return (0);
+
+	if (*input == ' ' || *input == '\t')
+		return (error_sep_op(input + 1, i + 1, last));
+
+	if (*input == ';')
+		if (last == '|' || last == '&' || last == ';')
+			return (i);
+
+	if (*input == '|')
+	{
+		if (last == ';' || last == '&')
+			return (i);
+
+		if (last == '|')
+		{
+			count = repeated_char(input, 0);
+			if (count == 0 || count > 1)
+				return (i);
+		}
+	}
+
+	if (*input == '&')
+	{
+		if (last == ';' || last == '|')
+			return (i);
+
+		if (last == '&')
+		{
+			count = repeated_char(input, 0);
+			if (count == 0 || count > 1)
+				return (i);
+		}
+	}
+
+	return (error_sep_op(input + 1, i + 1, *input));
+}
+
+/**
+* first_char - Find the first non-whitespace character in the input.
+* @input: Input string.
+* @i: Pointer to store the index of the first character.
+* Return: 0 if a non-separator character is found, -1 otherwise.
+*/
+int first_char(char *input, int *i)
+{
+	for (*i = 0; input[*i]; *i += 1)
+	{
+		if (input[*i] == ' ' || input[*i] == '\t')
+			continue;
+
+		if (input[*i] == ';' || input[*i] == '|' || input[*i] == '&')
+			return (-1);
+
+		break;
+	}
+
+	return (0);
+}
+
+/**
+* print_syntax_error - Print a syntax error message.
+* @datash: Shell data structure.
+* @input: Input string.
+* @i: Index where the error occurs.
+* @bool: Direction to determine the source of the error.
+*/
+void print_syntax_error(data_shell *datash, char *input, int i, int bool)
+{
+	char *msg, *msg2, *msg3, *error, *counter;
+	int length;
+
+	if (input[i] == ';')
+	{
+		if (bool == 0)
+			msg = (input[i + 1] == ';' ? ";;" : ";");
+		else
+			msg = (input[i - 1] == ';' ? ";;" : ";");
+	}
+
+	if (input[i] == '|')
+		msg = (input[i + 1] == '|' ? "||" : "|");
+
+	if (input[i] == '&')
+		msg = (input[i + 1] == '&' ? "&&" : "&");
+
+	msg2 = ": Syntax error: \"";
+	msg3 = "\"unexpected\n";
+	counter = aux_itoa(datash->counter);
+	length = _strlen(datash->av[0]) + _strlen(counter);
+	length += _strlen(msg) + _strlen(msg2) + _strlen(msg3) + 2;
+
+	error = malloc(sizeof(char) * (length + 1));
+	if (error == 0)
+	{
+		free(counter);
+		return;
+	}
+	_strcpy(error, datash->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, counter);
+	_strcat(error, msg2);
+	_strcat(error, msg);
+	_strcat(error, msg3);
+	_strcat(error, "\0");
+
+	write(STDERR_FILENO, error, length);
+	free(error);
+	free(counter);
+}
+
+/**
+* check_syntax_error - Check for syntax errors in the input string.
+* @datash: Shell data structure.
+* @input: Input string.
+* Return: 1 if a syntax error is found, 0 otherwise.
+*/
+int check_syntax_error(data_shell *datash, char *input)
+{
+	int begin = 0;
+	int f_char = 0;
+	int i = 0;
+
+	f_char = first_char(input, &begin);
+	if (f_char == -1)
+	{
+		print_syntax_error(datash, input, begin, 0);
+		return (1);
+	}
+
+	i = error_sep_op(input + begin, 0, *(input + begin));
+	if (i != 0)
+	{
+		print_syntax_error(datash, input, begin + i, 1);
+		return (1);
+	}
+
+	return (0);
+}
